c80bba54365eba9c786ae5ef43162c6bee782719
RustingSword <i@toonaive.me>
2025-02-17 15:09:50 +0800

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -9,12 +9,12 @@ fi
 umask g-w,o-w
 
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
-sed -i "s/^RPROMPT=.*/RPROMPT='\${return_status} \$(date \"+%F %T\")%{\$reset_color%}'/" $ZSH/themes/fishy.zsh-theme
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/larkery/zsh-histdb $HOME/.oh-my-zsh/custom/plugins/zsh-histdb
 
 cat << 'EOF' > ~/.zshrc
-export ZSH="/home/orz/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="fishy"
 plugins=(git z zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
